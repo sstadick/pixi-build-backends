b1e13c35a689f1b3974a51f95250c8712a99eb57
Seth Stadick <sstadick@gmail.com>
2025-07-26 11:05:04 +0000

    make target dir optional

--- crates/pixi-build-mojo/src/build_script.rs
+++ crates/pixi-build-mojo/src/build_script.rs
@@ -7,7 +7,7 @@ pub struct BuildScriptContext {
     /// The directory where the source code is located, the manifest root.
     pub source_dir: String,
     /// The directory name to place output artifacts, will be created in `source_dir`.
-    pub dist: String,
+    pub dist: Option<String>,
     /// Any executable artifacts to create.
     pub bins: Option<Vec<MojoBinConfig>>,
     /// Any packages to create.
